3ed3efad62c3ea7fdafcd29e488f4a2180b9b503
Michael <mkeating2225@gmail.com>
2017-11-14 17:50:31 -0500

    added router support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 import firebase from 'firebase';
-
 import Scroll from './components/Scroll';
+import Haikus from './components/Haikus';
 
 const config = require('./config.js');
 
@@ -22,9 +24,19 @@ class App extends Component {
       <div> 
         <h1>Renga</h1>
 
-          <Scroll db={firebase} />
+        <BrowserRouter>
+          <div>
+          <Route exact path="/"
+                  render={(props) => <Scroll db={firebase} />}
+          />
 
+          <Route path="/haikus"
+                  render={(props) => <Haikus db={firebase} />} 
+
+          />
           
+          </div>
+          </BrowserRouter>
 
       </div>
     );
---
+++ src/components/Haikus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Haikus extends Component {
+
+	render(){
+		return(
+			<div>All the current haikus</div>
+		)
+	}
+}
+
+export default Haikus;
