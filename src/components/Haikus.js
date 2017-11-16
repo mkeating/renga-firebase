ab68f0e771eff96d056d0822a81bba93340614e9
Michael <mkeating2225@gmail.com>
2017-11-16 18:57:45 -0500

    beginning to add animations

--- src/components/Haikus.js
+++ src/components/Haikus.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 
 class Haikus extends Component {
 
+	constructor(props) {
+		super(props);
+
+		let app = this.props.db.database().ref('haikus');
+
+		/*app.on('value', snapshot => {
+			this.getData(snapshot.val());
+		});*/
+	}
+
 	render(){
 		return(
 			<div>All the current haikus</div>
+			
+
+
 		)
 	}
 }
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -7,6 +7,9 @@ import InputBox from './InputBox';
 
 import _ from 'lodash';
 
+import { CSSTransitionGroup } from 'react-transition-group';
+
+import '../css/Scroll.css';
 
 
 
@@ -25,20 +28,17 @@ class Scroll extends Component {
 		});
 	}
 
-
-
 	getData(values) {
 		let messagesVal = values;
-		let messages = _(messagesVal)
-						.keys()
-						.map(messageKey => {
-							let cloned = _.clone(messagesVal[messageKey]);
-							cloned.key = messageKey;
-							return cloned;
-						})
-						.value();
+		
 
-		console.log(messages);
+		//maps the response from firebase to an array and assigns keys for use in the component
+		let messages = _(messagesVal).keys().map(key => {
+			let cloned = _.clone(messagesVal[key]);
+			cloned.key = key;
+			return cloned;
+		}).value();
+
 		this.setState({
 			messages: messages
 		});
@@ -55,18 +55,23 @@ class Scroll extends Component {
 
 		let messageNodes = this.state.messages.map(message => {
 			return (
-				<div>
+				
 					<Message message = {message.message} key={message.key} isHaiku={message.isHaiku}/>
-				</div>
 			);
 		});
 
 		return (
-			<div>
-				{messageNodes}
-
+				<div>
+				<div className="message-wrapper">
+				<CSSTransitionGroup
+			          transitionName="message"
+			          transitionEnterTimeout={500}
+			          transitionLeaveTimeout={500}>
+			          {messageNodes}
+			        </CSSTransitionGroup>
+			     </div>
 				<InputBox db={this.props.db} lastTwo={lastTwo} haikuCreated={this.haikuCreated()}/>
-			</div>
+				</div>
 		);
 	}
 }
